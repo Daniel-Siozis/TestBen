8dc8a2f1091601e54e7d6db5942c484b58411773
Daniel-Siozis <daniel.siozis@fau.de>
2024-05-21 19:03:28 +0200

    Get imagepaths with for

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Input, Conv2DTranspose
 from tensorflow.keras.optimizers import Adam
+import os
 from PIL import Image
 
 # Definiere dein vortrainiertes Modell, z.B. ein einfaches CNN
@@ -15,49 +16,36 @@ def create_model():
     return model
 
 # Lade deine 10 Bilder und ihre beschädigten Versionen
-def load_data():
+def load_data(train_path, noisy_path):
     # Hier lädst du deine Bilder und ihre beschädigten Versionen
-    train_images_paths = ['Dataset/9908/Probe/korrekt/9908_korrekt_2_f000000.jpg',
-                    'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000001.jpg',
-                     'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000002.jpg',
-                      'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000003.jpg',
-                       'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000004.jpg',
-                        'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000005.jpg',
-                         'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000006.jpg',
-                          'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000007.jpg',
-                           'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000008.jpg',
-                            'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000009.jpg',
-                             'Dataset/9908/Probe/korrekt/9908_korrekt_2_f000010.jpg']  # Liste mit den 10 Bildern
-    train_images_noisy_paths = ['Dataset/9908/Probe/broken/9908_broken_2_f000000.jpg',
-                    'Dataset/9908/Probe/broken/9908_broken_2_f000001.jpg',
-                     'Dataset/9908/Probe/broken/9908_broken_2_f000002.jpg',
-                      'Dataset/9908/Probe/broken/9908_broken_2_f000003.jpg',
-                       'Dataset/9908/Probe/broken/9908_broken_2_f000004.jpg',
-                        'Dataset/9908/Probe/broken/9908_broken_2_f000005.jpg',
-                         'Dataset/9908/Probe/broken/9908_broken_2_f000006.jpg',
-                          'Dataset/9908/Probe/broken/9908_broken_2_f000007.jpg',
-                           'Dataset/9908/Probe/broken/9908_broken_2_f000008.jpg',
-                            'Dataset/9908/Probe/broken/9908_broken_2_f000009.jpg',
-                             'Dataset/9908/Probe/broken/9908_broken_2_f000010.jpg']   # Liste mit den beschädigten Versionen der 10 Bilder
-    train_images = []
-    for path in train_images_paths:
-        image = Image.open(path)
-        image_array = np.array(image)
-        train_images.append(image_array)
+    valid_images_paths = []  # Liste mit den 10 Bildern
+    broken_images_paths = []   # Liste mit den beschädigten Versionen der 10 Bilder
 
-    train_images_noisy = []
-    for path in train_images_noisy_paths:
+    for filename in os.listdir(train_path):
+        img_path = os.path.join(train_path, filename)
+        valid_images_paths.append(img_path)
+
+    for filename in os.listdir(noisy_path):
+        img_path = os.path.join(noisy_path, filename)
+        broken_images_paths.append(img_path)
+
+    valid_images = []
+    for path in valid_images_paths:
+        image = Image.open(path)
+        valid_images.append(np.array(image))
+
+    broken_images = []
+    for path in broken_images_paths:
         noisy_image = Image.open(path)
-        noisy_image_array = np.array(noisy_image)
-        train_images_noisy.append(noisy_image_array)
+        broken_images.append(np.array(noisy_image))
 
     # Konvertiere die Listen in NumPy-Arrays und füge die Batch-Dimension hinzu
-    train_array_correct_dim = np.array(train_images)
-    train_array_broken_dim = np.array(train_images_noisy)
+    valid_train_images = np.array(valid_images)
+    broken_train_images = np.array(broken_images)
 
-    print(train_array_correct_dim.shape)
-    print(train_array_broken_dim.shape)
-    return train_array_correct_dim, train_array_broken_dim
+    print(valid_train_images.shape)
+    print(broken_train_images.shape)
+    return valid_train_images, broken_train_images
 
 # Definiere den Modelltrainingsprozess
 def train_model(model, train_images, train_images_noisy):
@@ -67,7 +55,9 @@ def train_model(model, train_images, train_images_noisy):
 # Hauptfunktion zum Trainieren und Speichern des Modells
 def main():
     # Lade Daten
-    train_images, train_images_noisy = load_data()
+    train_path = 'Dataset\lid'
+    noisy_path = 'Dataset\oken'
+    train_images, train_images_noisy = load_data(train_path, noisy_path)
 
     # Erstelle das Modell
     model = create_model()
